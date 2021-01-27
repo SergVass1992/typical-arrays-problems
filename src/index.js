05024dfc02da1b8b6bed1d83d6c12ad544ce7598
Сергей <56017574+SERgey1312@users.noreply.github.com>
2021-01-27 13:56:23 +0300

    feat: add function min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		let minimal = array[0];
+		for(let i = 0; i < array.length; i++){
+			if (array[i] < minimal) {
+				minimal = array[i];
+			}
+		}
+  		return minimal;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		let maximal = array[0];
+		for(let i = 0; i < array.length; i++){
+			if (array[i] > maximal) {
+				maximal = array[i];
+			}
+		}
+  	return maximal;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array == undefined || array.length == 0) {
+		return 0;
+	} else {
+		let sum = 0;
+		for(let i = 0; i < array.length; i++){
+			sum += array[i];
+		}
+ 		 return sum/array.length;
+	}
 }
+
